38ee5ff943bf0d2ffa94c35b54315ace1cc2af52
TheJeterLP <joey.peter1998@gmail.com>
2022-12-28 17:08:06 +0100

    add manager classes for readability

--- bot.js
+++ bot.js
@@ -1,40 +1,18 @@
 // Require the necessary discord.js classes
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+
+const cmdmanager = require('./CommandManager.js');
+const eventmanager = require('./EventManager.js');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+// Load events
+eventmanager.loadEvents(client);
 
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
-
-client.commands = new Collection();
-
-const commandsPaths = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPaths).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPaths, file);
-	const command = require(filePath);
-
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-}
+// Load commands
+cmdmanager.loadCommands(client);
 
 // Log in to Discord with your client token
 client.login(token);
